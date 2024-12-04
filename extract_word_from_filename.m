3938829e9cf54cc03f1de7bd0f7d08e6481096c7
prismaticoder <pjjsalam1999@gmail.com>
2024-12-04 18:43:38 +0000

    make word extractor more generic

--- extract_word_from_filename.m
+++ extract_word_from_filename.m
@@ -1,5 +1,5 @@
 function word = extract_word_from_filename(filename)
-% Extract word from filename format: sp01a_w03_head.mp3
+% Extract last word before .mp3 extension from filename (e.g., 'head' from 'sp01a_w03_head.mp3')
 parts = split(filename, '_');
-word = extractBefore(parts{3}, '.mp3');
+word = extractBefore(parts{end}, '.mp3');
 end
