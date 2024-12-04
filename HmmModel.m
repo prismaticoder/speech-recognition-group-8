68a51a0b1bafb204d5fc3e14f33ec746b2d57faa
Artem Karamyshev <artem.karamyshev@jetbrains.com>
2024-12-04 12:21:07 +0000

    update HmmModel to use log

--- HmmModel.m
+++ HmmModel.m
@@ -1,48 +1,48 @@
 classdef HmmModel < handle
     properties
-        word            % Word that this model represents (string)
-        A_matrix        % Transition matrix (NxN)
+        word           % Word that this model represents (string)
+        logA           % Transition matrix (NxN) in log space
         mu             % Mean vectors for each state (NxM)
-        sigma          % Covariance matrices for each state (MxMxN)
+        sigma          % Covariance matrices for each state (NxM)
         N              % Number of states
         M              % Number of observation symbols (e.g., MFCC dimensions)
-        pi             % Initial state distribution (Nx1)
-        eta            % Exit state distribution (Nx1)
+        logPi          % Initial state distribution (Nx1) in log space
+        logEta         % Exit state distribution (Nx1) in log space
     end
 
     methods
         % Constructor to initialize the model with word, A, mu, sigma
         function obj = HmmModel(word, A_matrix, mu, sigma, pi, eta)
             obj.word = word;
-            obj.A_matrix = A_matrix;
+            obj.logA = log(A_matrix);
             obj.mu = mu;
             obj.sigma = sigma;
             obj.N = size(A_matrix, 1);  % Number of states
             obj.M = size(mu, 2);  % Number of observation symbols (MFCC dimensions)
-            obj.pi = pi;
-            obj.eta = eta;
+            obj.logPi = log(pi);
+            obj.logEta = log(eta);
         end
 
-        % Method to compute observation probability using multivariate Gaussian
-        function prob = compute_observation_probability(obj, feature_vector, state)
-            % Debug prints
-            fprintf('Size of mu: %dx%d\n', size(obj.mu));
-            fprintf('Attempting to access state: %d\n', state);
-
-            mu_state = obj.mu(state, :);
-            sigma_state = obj.sigma(:, :, state);
-
-            % Compute multivariate Gaussian PDF
-            diff = feature_vector - mu_state;
-            exponent = -0.5 * diff * (sigma_state \ diff');
-            normalizer = sqrt((2*pi)^obj.M * det(sigma_state));
-            prob = exp(exponent) / normalizer;
+        function logProb = computeLogEmission(obj, x, state)
+            % Compute the log of the emission probability for a single observation and state
+            % Inputs:
+            %   x - Single observation vector (1xD)
+            %   mu - Mean vector for a specific state (1xD)
+            %   sigma - Standard deviation vector for a specific state (1xD)
+            % Outputs:
+            %   logProb - Logarithm of the emission probability (scalar)
+            stateMu = obj.mu(state, :);
+            stateSigma = obj.sigma(state, :);
+            D = length(x);
+            normalization = -0.5 * D * log(2 * pi) - sum(log(stateSigma)); % Gaussian normalization
+            exponent = -0.5 * sum(((x - stateMu) ./ stateSigma).^2); % Exponent term
+            logProb = normalization + exponent; % Log probability
         end
 
         % Method to estimate likelihood using Viterbi
         function likelihood = estimateLikelihood(obj, mfcc_features)
             % Debug prints
-            fprintf('Size of A_matrix: %dx%d\n', size(obj.A_matrix));
+            fprintf('Size of A_matrix: %dx%d\n', size(obj.logA));
             fprintf('Number of states (N): %d\n', obj.N);
             fprintf('Size of mfcc_features: %dx%d\n', size(mfcc_features));
 
@@ -55,8 +55,9 @@ classdef HmmModel < handle
 
             % Initialization (t=1) using pi
             for s = 1:N
-                viterbi(s, 1) = log(obj.pi(s)) + ...
-                    log(obj.compute_observation_probability(mfcc_features(1,:), s));
+                viterbi(s, 1) = obj.logPi(s) + ...
+                    obj.computeLogEmission(mfcc_features(1,:), s);
+                    % log(obj.compute_observation_probability(mfcc_features(1,:), s));
             end
 
             % Recursion using A_matrix directly
@@ -64,11 +65,12 @@ classdef HmmModel < handle
                 for s = 1:N
                     prev_probs = zeros(N, 1);
                     for prev_s = 1:N
-                        prev_probs(prev_s) = viterbi(prev_s, t-1) + log(obj.A_matrix(prev_s, s));
+                        prev_probs(prev_s) = viterbi(prev_s, t-1) + obj.logA(prev_s, s);
                     end
                     [max_val, best_prev_state] = max(prev_probs);
                     viterbi(s, t) = max_val + ...
-                        log(obj.compute_observation_probability(mfcc_features(t,:), s));
+                        obj.computeLogEmission(mfcc_features(t,:), s);
+                        % log(obj.compute_observation_probability(mfcc_features(t,:), s));
                     backpointer(s, t) = best_prev_state;
                 end
             end
@@ -76,7 +78,7 @@ classdef HmmModel < handle
             % Termination using eta
             final_probs = zeros(N, 1);
             for s = 1:N
-                final_probs(s) = viterbi(s, T) + log(obj.eta(s));
+                final_probs(s) = viterbi(s, T) + obj.logEta(s);
             end
             [likelihood, ~] = max(final_probs);
         end
